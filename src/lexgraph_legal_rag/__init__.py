497e374adc4e46605d5d05e73af70217712f8a9c
Daniel Schmidt <danschmidt88@gmail.com>
2025-06-22 21:32:57 -0400

    chore(release): prepare for release v0.0.1

--- src/lexgraph_legal_rag/__init__.py
+++ src/lexgraph_legal_rag/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.0.0"
+"""LexGraph Legal Retrieval Augmented Generation."""
+
+from .multi_agent import MultiAgentGraph, RouterAgent
+
+__version__ = "0.1.0"
+
+__all__ = ["MultiAgentGraph", "RouterAgent"]
